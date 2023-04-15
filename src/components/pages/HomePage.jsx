950f2df37eeaff8ba2281fe28111da14717749c6
ZhuravlevAlexei <110524860+ZhuravlevAlexei@users.noreply.github.com>
2023-04-15 17:09:49 +0300

    current

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,4 +1,14 @@
+import { getDataByAxios } from 'sevices/library';
+
 const HomePage = () => {
+  getDataByAxios(`/trending/movie/week`).then(resp =>
+    console.log('data ', resp.data)
+  );
+  // const getFilms = await axios.get(
+  //   `https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY}`
+  // );
+  // return getFilms.data;
+  //console.log('data ', data);
   return (
     <div>
       <h3>Trending today</h3>
--- src/sevices/library.jsx
+++ src/sevices/library.jsx
@@ -1,9 +1,19 @@
 import axios from 'axios';
 
-export async function getDataByAxios(searchString, page = 1) {
-  const URL_KEY = 'https://pixabay.com/api/';
-  const myTHEMOVIEDB_KEY = '32936589-73134fb91afb2b55fe07bd374';
-  const FILTER_PARAMETERS = `?key=${myPIXABAY_KEY}&q=${searchString}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=12`;
-  const resp = await axios.get(URL_KEY + FILTER_PARAMETERS);
+export async function getDataByAxios(PARAMETERS) {
+  const BASE_URL = `https://api.themoviedb.org/3`;
+  const MY_THEMOVIEDB_API_KEY = '6dfafd08c001fd3262dc3fe23504fa05';
+  const URL = BASE_URL + PARAMETERS + `?api_key=${MY_THEMOVIEDB_API_KEY}`;
+
+  console.log('URL ', URL);
+  const resp = await axios.get(URL);
   return resp;
 }
+
+// my themoviedb API keys
+//API Key (v3 auth)  6dfafd08c001fd3262dc3fe23504fa05
+
+//Example API Request  https://api.themoviedb.org/3/movie/550?api_key=6dfafd08c001fd3262dc3fe23504fa05
+
+//API Read Access Token (v4 auth)
+// eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZGZhZmQwOGMwMDFmZDMyNjJkYzNmZTIzNTA0ZmEwNSIsInN1YiI6IjY0M2E0OTk1MzEyMzQ1MDRjNGQ4MmNhNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.gg9n2ywUncYKufSDUyoeH5lMDCKTXBfCUwhmMMZYBBk
