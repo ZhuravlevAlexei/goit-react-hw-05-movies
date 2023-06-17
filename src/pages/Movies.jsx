9c5c866b09990cf44cfbe41cbb5b7e29b7dc795f
ZhuravlevAlexei <110524860+ZhuravlevAlexei@users.noreply.github.com>
2023-06-17 21:48:39 +0300

    search mistakes

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -7,15 +7,15 @@ import MovieList from 'components/MovieList/MovieList';
 const Movies = () => {
   const location = useLocation();
   const [searchParams, setSearchParams] = useSearchParams();
-  // !!don't erase this line!! console.log('searchParams :>> ', Object.fromEntries([...searchParams]));
+  //console.log('searchParams :>> ', Object.fromEntries([...searchParams]));
+
   const [movieList, setMovieList] = useState([]);
+  const [searchText, setSearchText] = useState(
+    searchParams.get('search') ?? ''
+  );
   const totalPages = useRef(0);
 
-  let paginationPage = Number(searchParams.get('page'));
-  if (paginationPage === 0) {
-    paginationPage = 1;
-  }
-  let searchText = searchParams.get('search') ?? '';
+  let paginationPage = Number(searchParams.get('page')) ?? 0;
   let title = '';
 
   useEffect(() => {
@@ -23,7 +23,6 @@ const Movies = () => {
       if (resp.status !== 200) {
         throw new Error(resp.statusText);
       } else {
-        // setTotalPages(resp.data.total_pages);
         totalPages.current = resp.data.total_pages;
         setMovieList(resp.data.results);
       }
@@ -33,7 +32,9 @@ const Movies = () => {
   const handleSubmit = evt => {
     evt.preventDefault();
     paginationPage = 1;
-    setSearchParams({ search: searchText.trim(), page: 1 });
+    let localValue = searchParams.get('search');
+    setSearchParams({ search: localValue.trim(), page: 1 });
+    setSearchText(localValue.trim());
   };
 
   const handleSearchInputChange = ({ target: { value } }) => {
@@ -66,7 +67,7 @@ const Movies = () => {
       <Search
         handleSubmit={handleSubmit}
         handleSearchInputChange={handleSearchInputChange}
-        searchText={searchText}
+        searchText={searchParams.get('search') ?? ''}
       />
       {searchText && <h3>{title}</h3>}
       {movieList.length !== 0 && (
